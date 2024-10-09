fedf120ada6dd090690649063aa7052a8f1ff623
gnickel <gnickel@student.42heilbronn.de>
2024-10-09 16:19:25 +0200

    Added memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: gnickel <gnickel@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:17:47 by gnickel           #+#    #+#             */
-/*   Updated: 2024/10/09 15:17:47 by gnickel          ###   ########.fr       */
+/*   Updated: 2024/10/09 16:00:33 by gnickel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
+	size_t		i;
+	char		*dst_peter;
+	const char	*src_peter;
 
+	i = 0;
+	dst_peter = dst;
+	src_peter = src;
+	if (!dst && !src)
+		return (dst);
+	while (i < n)
+	{
+		dst_peter[i] = src_peter[i];
+		i++;
+	}
+	return (dst_peter);
 }
