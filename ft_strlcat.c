f282b31a38ad203d9f78a01f0a501a6a804561e2
gnickel <gnickel@student.42heilbronn.de>
2024-10-10 13:14:25 +0200

    Added strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gnickel <gnickel@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 11:10:00 by gnickel           #+#    #+#             */
+/*   Updated: 2024/10/10 11:10:00 by gnickel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	while (dst[i] && i < dstsize)
+		i++;
+	return (ft_strlcpy(&dst[i], src, dstsize - i) + i);
+}
