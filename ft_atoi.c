52bf1c2ef75758925315efdee83b682a5dd9b2af
gnickel <gnickel@student.42heilbronn.de>
2024-10-11 14:34:18 +0200

    Added atoi

---
+++ ft_atoi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gnickel <gnickel@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 13:21:04 by gnickel           #+#    #+#             */
+/*   Updated: 2024/10/11 13:21:04 by gnickel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	size_t	i;
+	int		sign;
+	int		res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' \
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	while (ft_isdigit(str[i]))
+		res = res * 10 + (str[i++] - '0');
+	return (res * sign);
+}
