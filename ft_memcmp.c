279a072034a1dae539547ec8a72eacd367b046ec
gnickel <gnickel@student.42heilbronn.de>
2024-10-11 10:59:00 +0200

    Added memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gnickel <gnickel@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 10:45:12 by gnickel           #+#    #+#             */
+/*   Updated: 2024/10/11 10:45:12 by gnickel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t				i;
+	const unsigned char	*s1_ptr;
+	const unsigned char	*s2_ptr;
+
+	i = 0;
+	s1_ptr = s1;
+	s2_ptr = s2;
+	while (i < n)
+	{
+		if (s1_ptr[i] != s2_ptr[i])
+			return (s1_ptr[i] - s2_ptr[i]);
+		i++;
+	}
+	return (0);
+}
