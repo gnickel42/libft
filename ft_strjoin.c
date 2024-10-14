6202ae624d70360ece5b7deefaf42fa73624f08b
gnickel <gnickel@student.42heilbronn.de>
2024-10-14 13:50:12 +0200

    Added strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gnickel <gnickel@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 13:18:51 by gnickel           #+#    #+#             */
+/*   Updated: 2024/10/14 13:18:51 by gnickel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dst;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	s1_len = ft_strlen(s1) + 1;
+	s2_len = ft_strlen(s2) + 1;
+	dst = malloc(s1_len + s2_len - 1);
+	if (dst)
+	{
+		ft_strlcpy(dst, s1, s1_len);
+		ft_strlcat(dst, s2, s1_len + s2_len);
+		return (dst);
+	}
+	return (0);
+}
