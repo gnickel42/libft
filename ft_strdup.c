51c36c1da9e8f0afe42ce366c4e62aaab00a6a7f
gnickel <gnickel@student.42heilbronn.de>
2024-10-14 12:38:09 +0200

    Added strdup and changed bzero

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,18 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gnickel <gnickel@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/09 15:08:46 by gnickel           #+#    #+#             */
-/*   Updated: 2024/10/09 15:08:46 by gnickel          ###   ########.fr       */
+/*   Created: 2024/10/14 12:23:26 by gnickel           #+#    #+#             */
+/*   Updated: 2024/10/14 12:23:26 by gnickel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t n)
+char	*ft_strdup(const char *s1)
 {
-	ft_memset(s, 0, n);
+	char	*dst;
+	size_t	i;
+
+	i = 0;
+	dst = malloc(ft_strlen(s1) + 1);
+	if (dst)
+	{
+		while (i < ft_strlen(s1))
+		{
+			dst[i] = s1[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
+	return (dst);
 }
