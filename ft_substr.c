a359b1b431dd92a78588b3c515d54c85a782a764
gnickel <gnickel@student.42heilbronn.de>
2024-10-14 13:18:14 +0200

    Added substr

---
+++ ft_substr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gnickel <gnickel@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 12:42:40 by gnickel           #+#    #+#             */
+/*   Updated: 2024/10/14 12:42:40 by gnickel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*dst;
+	size_t	i;
+
+	i = 0;
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (start + len > ft_strlen(s))
+		len = ft_strlen(s) - start;
+	dst = malloc(len + 1);
+	if (dst)
+	{
+		while (i < len && s[start])
+		{
+			dst[i] = s[start];
+			i++;
+			start++;
+		}
+		dst[i] = 0;
+		return (dst);
+	}
+	return (0);
+}
