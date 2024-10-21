76e07bf2f3e2370df50307fc9b7119eb2291ec7a
gnickel <gnickel@student.42heilbronn.de>
2024-10-21 11:55:29 +0200

    continued split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: gnickel <gnickel@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 11:23:17 by gnickel           #+#    #+#             */
-/*   Updated: 2024/10/18 13:13:06 by gnickel          ###   ########.fr       */
+/*   Updated: 2024/10/21 10:34:21 by gnickel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,12 @@ static size_t	word_count(const char *s, char c)
 	w_count = 0;
 	while (s[i])
 	{
+		if (c == '\0')
+			break;
 		if (s[i] != c && (i == 0 || s[i - 1] == c))
 			w_count++;
 		i++;
+		// printf("i: %zu, w: %zu\n", i, w_count);
 	}
 	return (w_count);
 }
@@ -41,7 +44,7 @@ static char	*sp_helper(const char *s, char c, char **dst)
 	{
 		if (s[i] != c && (i == 0 || s[i - 1] == c))
 			start = i;
-		if (s[i] != c && (s[i + 1] == c || !s[i + 1]))
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
 		{
 			dst[sub_i] = ft_substr(s, start, i - start + 1);
 			if(!dst[sub_i])
@@ -69,21 +72,7 @@ static void	sp_free(char **split)
 		free(split[i]);
 		i++;
 	}
-	free(split[i]);
-}
-
-static int	empty_check(const char *s, char c)
-{
-	size_t	i;
-
-	i = 1;
-	while (s[i])
-	{
-		if (s[i] != c)
-			return (0);
-		i++;
-	}
-	return (1);
+	free(split);
 }
 
 char	**ft_split(const char *s, char c)
@@ -91,14 +80,14 @@ char	**ft_split(const char *s, char c)
 	char	**dst;
 	size_t	words;
 
-	words = word_count(s, c);
-	if (empty_check(s, c) == 1)
-		return ((char **)ft_strdup("s"));
 	if (!s)
 		return (NULL);
+	if (c == '\0')
+        return (dst = ft_calloc(1, sizeof(char *)));
+	words = word_count(s, c);
+	if (s[0] == '\0' || words == 0)
+		return (dst = ft_calloc(1, sizeof(char *)));
 	dst = (char **)malloc((words + 1) * sizeof(char *));
-	if (words == 0)
-		return((char **)ft_strdup(""));
 	if (!dst)
 		return (NULL);
 	if (!sp_helper(s, c, dst))
@@ -110,7 +99,7 @@ char	**ft_split(const char *s, char c)
 }
 
 // int	main(void) {
-// 	char **dst = ft_split("          ", ' ');
+// 	char **dst = ft_split("nonempty", 0:'\0');
 // 	printf("result[0] = '%s'\n", dst[0]);
 // 	printf("result[1] = %p\n", dst[1]); // Should be NULL
 // 	for (size_t i = 0; ; i++) {
