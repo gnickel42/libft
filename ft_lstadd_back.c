73b7a71fcbc0496c86cdac823a29b3b7418756bb
gnickel <gnickel@student.42heilbronn.de>
2024-10-22 16:03:12 +0200

    added lstadd_back

---
+++ ft_lstadd_back.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gnickel <gnickel@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 15:58:38 by gnickel           #+#    #+#             */
+/*   Updated: 2024/10/22 15:58:38 by gnickel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	last = *lst;
+	if (!last)
+		*lst = new;
+	else
+	{
+		while (last->next)
+			last = last->next;
+		last->next = new;
+	}
+}
